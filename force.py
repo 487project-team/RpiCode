f6c2f15633a07951701b1d9c6de019ad055af6d2
Andrew <71098842+AuxiliaryPanther@users.noreply.github.com>
2020-12-05 19:11:10 -0500

    Update force.py
    Condensed/cleaned up code. Works with two webcams. No PiCam additions or grid-style layout yet.

--- force.py
+++ force.py
@@ -1,21 +1,16 @@
+#!/usr/bin/env python3
 #Dependencies
-
-import sys
-import cv2
-import time
-import serial
-import threading
+import sys, cv2, time, serial, threading, time
 import tkinter as tk
 from tkinter import *
 import tkinter.ttk as ttk
 from queue import Queue
 from PIL import Image
 from PIL import ImageTk
-import time
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=None)
-global limit
-limit = "L"
+global limit; limit = "L"
+global No; No = 0
 
 #Class For Camera Control and Display
 class App(tk.Frame):
@@ -64,22 +59,17 @@ class App(tk.Frame):
         self.view1.pack(side=tk.RIGHT, expand=True)
         self.button_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        #self.bind("<e>", self.force_extend)
-        #self.bind("<r>", self.force_retract)
-        #self.bind("<f>", self.low_limit)
-        #self.bind("<a>", self.high_limit)
-
     def bottom_ui(self):
         self.button_frameB = tk.Frame(self)
         self.Hbutton = Button(text="Aluminum", width=10, bg="gray", fg="red", command=self.high_limit)
         self.Hbutton.pack(side=RIGHT)
-        self.Lbutton = Button(text="Fiberglass", width=10, bg="gray", fg="green", command=self.low_limit)
+        self.Lbutton = Button(text="Fiberglass", width=10, bg="alice blue", fg="green", command=self.low_limit)
         self.Lbutton.pack(side=RIGHT)
         self.Rbutton = Button(text="Retract", width=10, bg="gray", fg="red", command=self.force_retract)
         self.Rbutton.pack(side=LEFT)
         self.Ebutton = Button(text="Extend", width=10, bg="gray", fg="green", command=self.force_extend)
         self.Ebutton.pack(side=LEFT)
-        self.Cbutton = Button(text="Reset Serial Communications with Arduino", width=30, bg="gray", fg="white", command=self.reset_comms)
+        self.Cbutton = Button(text="Start Arduino Serial Comms", width=30, bg="gray", fg="white", command=self.start_comms)
         self.Cbutton.pack(side=BOTTOM)
         self.forceStatus = Label(text="Linear Actuator Status",width=30, height=4)
         self.forceStatus.pack(side=BOTTOM)
@@ -97,23 +87,15 @@ class App(tk.Frame):
             self.thread.join(0.2)
         self.winfo_toplevel().destroy()
 
-    def start(self,CamNum):
-        global No
-        No = CamNum
-        print(No)
-        #No=0
+    def start(self, No):
         if No == 0:
             self.is_running0 = True
             self.thread0 = threading.Thread(target=self.videoLoop0, args=(),daemon=True).start()
         else:
             self.is_running1 = True
             self.thread1 = threading.Thread(target=self.videoLoop1, args=(),daemon=True).start()
-        #self.thread.daemon = True
-        #self.thread.start()
 
-    def stop(self, CamNum):
-        global No
-        No = CamNum
+    def stop(self, No):
         if No == 0:
             self.photo0 = ImageTk.PhotoImage(Image.new("RGB", (64, 48), "teal"))
             self.is_running0 = False
@@ -123,8 +105,6 @@ class App(tk.Frame):
 
 #####Onboard Camera video feed, for different camera change No=0,1,2,n....    
     def videoLoop0(self, mirror=False):
-        #global No
-        #if No == 0:
         cap0 = cv2.VideoCapture(No)
         cap0.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
         cap0.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
@@ -137,13 +117,10 @@ class App(tk.Frame):
             self.queue0.put(image0)
             self.event_generate('<<MessageGenerated>>')
             
-        #else:
     def videoLoop1(self, mirror=False):
         cap1 = cv2.VideoCapture(No)
         cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
         cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
-        
-        # before splitting videoLoop up, the #1 stuff was here.
         
         while self.is_running1:
             ret1, cam1 = cap1.read()
@@ -167,40 +144,33 @@ class App(tk.Frame):
             self.view1.configure(image=self.photo1)
 
     def low_limit(self):
-        global limit
         limit = "L"
         self.Lbutton.config(relief=SUNKEN, bg = "alice blue")
         self.Hbutton.config(relief=RAISED, bg = "gray")
         
-    #self.Lbutton = Button(text="Fiberglass", width=10, bg="gray", fg="green", command=self.low_limit)
-   
     def high_limit(self):
-        global limit
         limit = "H"
         self.Hbutton.config(relief=SUNKEN, bg = "alice blue")
         self.Lbutton.config(relief=RAISED, bg = "gray")
         
-    def reset_comms_needle(self):
-        while ser.in_waiting:
-            global forceimport
-            forceimport = str(ser.readline().decode('utf_8')).replace("\r\n", "")
-            self.forceStatus["text"] = "Linear Actuator "+ forceimport[0:7]
-            self.forceReading["text"] = "Load Cell Reads " + forceimport[7:] + " lbs"
-        time.sleep(0.1)
-        self.reset_comms_needle()
+    def start_comms_needle(self):
+        while True:
+            if ser.in_waiting:
+                global forceimport
+                forceimport = str(ser.readline().decode('utf_8')).replace("\r\n", "")
+                self.forceStatus["text"] = "Linear Actuator "+ forceimport[0:7]
+                self.forceReading["text"] = "Load Cell Reads " + forceimport[7:] + " lbs"
+            time.sleep(0.1)
+
     
-    def reset_comms(self):
-        threading.Thread(target=self.reset_comms_needle, args=(), daemon=True).start()
+    def start_comms(self):
+        threading.Thread(target=self.start_comms_needle, args=(), daemon=True).start()
     
     def force_retract_needle(self):
-        #global forcecommand
-        global limit
         forcecommand = limit + "R"
         ser.write(str(forcecommand).encode('utf_8'))
         
     def force_extend_needle(self):
-        #global forcecommand
-        global limit
         forcecommand = limit + "E"
         ser.write(str(forcecommand).encode('utf_8'))
 
@@ -218,6 +188,7 @@ class App(tk.Frame):
         self.low_limit()
     def high_limit_key(self, eventargs):
         self.high_limit()
+
 #def main(args):
 root = tk.Tk()
 
@@ -231,15 +202,11 @@ app = App(root, "Force Control Menu")
     # forceLabel.grid(column=0,row=3)
     # forceReading.grid(column=0,row=4)
         
-    #Key binds control everything
-
-
 #Controls Menu Settings
 top = Toplevel()
 top.title("About this application...")
 msg = Message(top, text="Place Holder Instructions")
 msg.pack()
-
 button = Button(top, text="Dismiss", command=top.destroy)
 button.pack()
     
